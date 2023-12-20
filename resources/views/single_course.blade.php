18676f7bbf668a8952e7f00cd90b6bc203c799b6
@parou.char <curlycode@outlook.com>
2023-12-21 01:37:30 +0200

    running migrations for students

--- resources/views/single_course.blade.php
+++ resources/views/single_course.blade.php
@@ -6,29 +6,25 @@
 
 
 <div class="container mx-auto">
-<div class="text-3xl p-5 text-gray-800 ">    
-<h1>My dashboard</h1>
-</div>
+    <div class="text-3xl p-5 text-gray-800 ">
+        <h1>My dashboard</h1>
+    </div>
 
-<article class="container">
-    <div class="mx-auto bg-gray-100 text-center w-2/4 p-5 mt-5">
-    <h1 class="text-gray-700 text-2xl font-semibold">
-        <a href="/courses/{{$course->slug}}">
-            {!! $course->title !!}
-        </a>
-    </h1>
-    <h3 class="mt-5 w-full text-left mx-auto">
-        {!! $course->body !!}
-</h3>
-</div>  
-<div>
-<p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>{{$course->created_at->diffForHumans()}}</time>
-</p>
-</div>
-</article>
-
-
-
-
+    <article class="container">
+        <div class="mx-auto bg-gray-100 text-center w-2/4 p-5 mt-5">
+            <h1 class="text-gray-700 text-2xl font-semibold">
+                <a href="/courses/{{$course->slug}}">
+                    {!! $course->title !!}
+                </a>
+            </h1>
+            <h3 class="mt-5 w-full text-left mx-auto">
+                {!! $course->body !!}
+            </h3>
+        </div>
+        <div>
+            <p class="mt-4 block text-gray-400 text-xs">
+                Published <time>{{$course->created_at->diffForHumans()}}</time>
+            </p>
+        </div>
+    </article>
 </div>
