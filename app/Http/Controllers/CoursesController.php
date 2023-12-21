c318117a7e1913e179237b5e988fe2c65783afa4
@parou.char <curlycode@outlook.com>
2023-12-21 19:47:33 +0200

    implementing views for enrollment students

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symphony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation;
 use App\Models\Courses;
+use App\Models\Students;
 class CoursesController extends Controller
 {
     public function index(){
         $courses = Courses::latest()->get();
+        
         return view('courses',['courses' => $courses]);
     }
 
     public function show(Courses $course){
-       
-        return view('single_course', ['course' => $course]);
+       $students = Students::latest()->get();
+        return view('single_course', [
+            'course' => $course,
+            'students' => $students
+        ]);
     }
 }
--- resources/views/single_course.blade.php
+++ resources/views/single_course.blade.php
@@ -6,25 +6,62 @@
 
 
 <div class="container mx-auto">
-    <div class="text-3xl p-5 text-gray-800 ">
+    <div class="text-3xl p-3 text-gray-800 ">
         <h1>My dashboard</h1>
     </div>
 
     <article class="container">
-        <div class="mx-auto bg-gray-100 text-center w-2/4 p-5 mt-5">
+        <div class="mx-auto bg-gray-100 text-center w-full p-4 mt-5 mb-10">
             <h1 class="text-gray-700 text-2xl font-semibold">
                 <a href="/courses/{{$course->slug}}">
-                    {!! $course->title !!}
+                    {{ $course->title }}
                 </a>
             </h1>
             <h3 class="mt-5 w-full text-left mx-auto">
-                {!! $course->body !!}
+                {{ $course->body }}
             </h3>
         </div>
-        <div>
-            <p class="mt-4 block text-gray-400 text-xs">
-                Published <time>{{$course->created_at->diffForHumans()}}</time>
-            </p>
-        </div>
+
     </article>
+    <div class="flex">
+        <div class="container mx-auto ">
+            <h1 class="text-xl  text-left mb-10">Enrolled Completed Students</h1>
+            @foreach($students as $student)
+            <div class="flex flex-column bg-gray-50 w-2/4 p-2 justify-center items-center">
+
+                <div class="flex flex-row items-center justify-around  text-left mt-0  ">
+
+                    <p class=" text-left text-sm">
+                        @if($student->completion === "Y")
+                    <h3 class="text-left w-full ">{{$student->name}}</h3>
+                    <p class="mx-6 text-left text-sm">{{$student->email}}</p>
+                    <div class=" text-sm text-gray-800 bg-green-200 w-full p-1">Completed</div>
+                    @endif
+                    </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+
+        <div class="container mx-auto ">
+            <h1 class="text-xl  text-left mb-5">Enrolled Students</h1>
+            @foreach($students as $student)
+            <div class="flex flex-column bg-gray-50 w-2/3 p-2 justify-center items-center">
+
+                <div class="flex flex-row items-center justify-around  text-left mt-2  ">
+
+                    <p class="mx-6 text-left text-sm">
+                    <h3 class="text-left w-1/3  ">{{$student->name}}</h3>
+                    <p class="mx-6 text-left text-sm">{{$student->email}}</p>
+
+
+                    </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+
 </div>
