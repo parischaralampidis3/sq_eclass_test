22c35925c77ea27f56750dce4ff686ca1f800e5e
@parou.char <curlycode@outlook.com>
2023-12-25 22:22:22 +0200

    making fixes at the elronment logic for students 0.2

--- resources/views/single_course.blade.php
+++ resources/views/single_course.blade.php
@@ -20,26 +20,27 @@
         </div>
     </article>
 
-    <div class="flex flex-col md:flex-row">
-        <div class="container mx-auto w-full md:w-4/5 mb-8 md:mb-0">
-            <h1 class="text-xl text-left mb-3">Enrolled Completed Students</h1>
+    <div class="flex flex-col md:flex-row"> 
+<div class="flex flex-col">
+          <h1 class="text-xl text-left mb-3">Enrolled Completed Students</h1>
+        <div class="container bg-gray-50  mx-auto w-full md:w-4/5 mb-0 md:mb-0">
+       
             @foreach($students as $student)
-            <div class="flex flex-col bg-gray-50 p-2 justify-center items-center mb-2">
+            <div class="flex flex-col p-1 justify-center items-center mb-0">
                 <div class="flex flex-col text-center mt-0">
                     <p class="text-left text-sm">
-                        @if($student->completion === "Y" && $student->enrollment=="Y")
+                        @if($student->completion === "Y" && $student->enrollment==="Y")
                         <h3 class="text-lg w-full ">{{$student->name}}</h3>
                         <p class="mx-0 text-left text-sm">{{$student->email}}</p>
                         <div class="text-sm text-gray-800 bg-green-200 w-full p-1">Completed</div>
-                        @else
-                        <p class="hidden">no records</p>
+                     
                         @endif
                     </p>
                 </div>
             </div>
             @endforeach
         </div>
-
+</div>
         <div class="container mx-auto w-full md:w-3/5">
             <h1 class="text-xl text-left mb-5">Enrolled Students</h1>
             @foreach($students as $student)
