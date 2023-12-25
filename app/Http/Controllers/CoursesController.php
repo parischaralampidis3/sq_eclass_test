8d94d1ad9cb29fa47bcb52b8c9bcd6fb88595cd9
@parou.char <curlycode@outlook.com>
2023-12-25 11:59:23 +0200

    implementing connection through the repository and the controller

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Courses;
+use App\Repositories\CoursesRepository;
 
 /**
  * Courses Controller defines the logic for displaying courses with enrolled students.
@@ -15,12 +16,29 @@ use App\Models\Courses;
  * THe show() method provides results based in the a relationship between courses and enrolled students.
  * More specifically, a collection of enrolled students is provided, that is related to the courses.
  */
+
+ /**
+  * Implementing courses repository binding at the controller level.
+  * Adding a constructor at the class and passing it, coursesRepository, I try to inject 
+  * controller preffered methods into the constructor and reository.
+  */
+
+  
+
 class CoursesController extends Controller
 {
+    private $coursesRepository;
+
+    public function __construct(CoursesRepository $coursesRepository ){
+        $this -> coursesRepository = $coursesRepository;
+    }
+
     public function index()
     {
-        $courses = Courses::latest()->get();
-        return view('courses', ['courses' => $courses]);
+        $courses = $this -> coursesRepository->get();
+
+        return $courses;
+     
     }
 
     public function show(Courses $course)
--- app/Repositories/CoursesRepository.php
+++ app/Repositories/CoursesRepository.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Repositories;
-
+use App\Models\Courses;
 
 class CoursesRepository
 {
+public function get(){
 
-    
+        $courses = Courses::latest()->get();
+        return view('courses', ['courses' => $courses]);
+}
+
 }
