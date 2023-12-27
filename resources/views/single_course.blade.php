67dcf96be94cc34e962cb19889a4c5b8cca25822
@parou.char <curlycode@outlook.com>
2023-12-27 06:26:00 +0200

    modifying single course view

--- resources/views/single_course.blade.php
+++ resources/views/single_course.blade.php
@@ -9,10 +9,8 @@
 
     <article class="container">
         <div class="mx-auto bg-gray-100 text-center w-full p-10 mt-5 mb-10">
-            <h1 class="text-gray-700 text-lg text-left mx-24 font-semibold">
-                <a href="/courses/{{$course->slug}}">
+            <h1 class="text-gray-700 text-2xl text-left mx-24 font-semibold">
                     {{ $course->title }}
-                </a>
             </h1>
             <h3 class="mt-5 w-3/4 text-base text-left mx-24">
                 {{ $course->body }}
@@ -20,10 +18,10 @@
         </div>
     </article>
 
-    <div class="flex flex-col md:flex-row"> 
+    <div class="flex flex-col md:flex-row p-4"> 
 <div class="flex flex-col">
           <h1 class="text-xl text-left mb-3">Enrolled Completed Students</h1>
-        <div class="container bg-gray-50  mx-auto w-full md:w-4/5 mb-0 md:mb-0">
+        <div class="container bg-gray-50 p-2 mx-auto w-full md:w-4/5 mb-0 md:mb-0">
        
             @foreach($students as $student)
             <div class="flex flex-col p-1 justify-center items-center mb-0">
@@ -32,7 +30,7 @@
                         @if($student->completion === "Y" && $student->enrollment==="Y")
                         <h3 class="text-lg w-full ">{{$student->name}}</h3>
                         <p class="mx-0 text-left text-sm">{{$student->email}}</p>
-                        <div class="text-sm text-gray-800 bg-green-200 w-full p-1">Completed</div>
+                        <div class="text-base bg-green-500 w-full text-white font-semibold p-1">Completed</div>
                      
                         @endif
                     </p>
@@ -41,18 +39,38 @@
             @endforeach
         </div>
 </div>
-        <div class="container mx-auto w-full md:w-3/5">
+        <div class="container mx-auto w-full md:w-1/3">
             <h1 class="text-xl text-left mb-5">Enrolled Students</h1>
-            @foreach($students as $student)
-            <div class="flex flex-col bg-gray-50 p-2 justify-center items-center mb-2">
-                <div class="flex flex-row items-center justify-around text-left mt-2">
-                    <p class="text-left text-sm">
-                        <h3 class="text-base w-2/3">{{$student->name}}</h3>
-                        <p class="mx-6 text-left text-sm">{{$student->email}}</p>
-                    </p>
+          <div class="block w-full">
+                    <table class="items-center bg-transparent w-full border-collapse sm:table-auto">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Name
+                                </th>
+                                <th
+                                    class="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Email
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($students as $student)
+                            <tr>
+                                <td
+                                    class="border-t-0 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                    {!! $student->name !!}
+                                </td>
+                                <td
+                                    class="border-t-0 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                    {!! $student->email !!}
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-            </div>
-            @endforeach
         </div>
     </div>
 </div>
