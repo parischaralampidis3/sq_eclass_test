f10bc3d0c3a4c4cad038cd75f53202d726b04577
@parou.char <curlycode@outlook.com>
2023-12-27 10:58:24 +0200

    making microimprovments 0.2

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand mx-12" href="/">E-class</a>
+    <a class="navbar-brand mx-12gi" href="/">E-class</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
